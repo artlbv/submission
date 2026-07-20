8fd2990b3483ac0951d5ddef6bf2a828dd1905dc
Artur Lobanov <Artur.Lobanov@cern.ch>
2026-07-20 22:33:39 +0200

    add miss file

---
+++ templates/crab_NTP_TwoDatasets.py
@@ -0,0 +1,47 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# config.General.requestName = 'SingleGammaPt25Eta1p6_2p8_PU0'
+# config.Data.inputDataset = '/SingleGammaPt25Eta1p6_2p8/PhaseIITDRFall17DR-noPUFEVT_93X_upgrade2023_realistic_v2-v1/GEN-SIM-DIGI-RAW'
+
+config.General.requestName = 'TEMPL_REQUESTNAME'
+config.Data.inputDataset = 'TEMPL_INPUTDATASET'
+config.Data.secondaryInputDataset = 'TEMPL_SECONDINPUTDATASET'
+
+config.General.workArea = 'TEMPL_TASKDIR'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'TEMPL_TASKCONFDIR/input_cfg.py'
+config.JobType.maxMemoryMB = 4000
+config.JobType.numCores = 1
+
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'TEMPL_SPLITTINGMODE'
+config.Data.unitsPerJob = TEMPL_SPLITGRANULARITY
+config.Data.totalUnits = TEMPL_NEVENTS
+config.Data.outLFNDirBase = 'TEMPL_CRABOUTDIR'
+config.Data.publication = False
+config.Data.ignoreLocality = False
+config.Data.outputDatasetTag = 'TEMPL_DATASETTAG'
+
+config.Data.partialDataset = True
+
+config.Site.storageSite = 'T2_CH_CERN'
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.allowNonValidInputDataset = True
+
+# restrict which sites CRAB may use (only these sites will be considered)
+config.Site.whitelist = [
+    'T2_DE_DESY', 
+    'T2_CH_CERN',  
+    'T1_FR_CCIN2P3',
+]
+
+# optionally blacklist problematic sites you saw in logs
+config.Site.blacklist = [
+    'T1_RU_JINR', 
+    'T1_US_FNAL', 
+]
